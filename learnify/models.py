c76df918f352e9cc757681a4987c0612c651187e
Shann404 <shannbahati@gmail.com>
2024-12-05 23:16:04 +0300

    models.py modified

--- learnify/models.py
+++ learnify/models.py
@@ -9,6 +9,8 @@ class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
