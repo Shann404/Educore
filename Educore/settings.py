c18a1dfefd8ce0cf79fd630be6d95f9d9d054766
Shann404 <shannbahati@gmail.com>
2025-06-02 16:47:04 +0300

    changed MYSQL TO sqlite3

--- Educore/settings.py
+++ Educore/settings.py
@@ -82,14 +82,11 @@ WSGI_APPLICATION = 'Educore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Educore',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
